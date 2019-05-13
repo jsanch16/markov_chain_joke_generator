087e20d337d1634d9b9e7598910e3f4217e3ea2a
Juan Sanchez <juansanchez3096@sbcglobal.net>
2019-05-12 17:43:46 -0700

    added explanatory comments

--- app/commands/markov_chain_generate.rb
+++ app/commands/markov_chain_generate.rb
@@ -2,6 +2,8 @@ class MarkovChainGenerate
   prepend SimpleCommand
 
 
+  # Start by constructing frequency hash of the numer of times seen (frequency)
+  # of each word and its next word. Example hash:
   # {
   #   "word1": { 
   #     "next_word1": 2,
@@ -16,7 +18,6 @@ class MarkovChainGenerate
     @text = text
     @word_frequencies = {}
     word_array = text.split(" ")
-    #split and pass each two adjacent words to add
     word_array.each_cons(2) do |word, next_word|
       @word_frequencies[word] ||= Hash.new(0)
       @word_frequencies[word][next_word] += 1
@@ -42,10 +43,14 @@ class MarkovChainGenerate
   private
   attr_accessor :text, :word_frequencies
 
+  # Receives current word and grabs next word by getting the frequencies of
+  # every potential next word and summing them up and making a random selection
+  # of that range. That selection is then used to grab the word in the
+  # frequencies hash.
   def get_next_word(word)
     return "" if !word_frequencies.key?(word)
     pot_next_words = word_frequencies[word]
-    sum = pot_next_words.inject(0) {|sum,kv| sum += kv[1]}
+    sum = pot_next_words.inject(0) {|sum,words| sum += words[1]}
     random = rand(sum)+1
     partial_sum = 0
     next_word = pot_next_words.find do |word, count|
